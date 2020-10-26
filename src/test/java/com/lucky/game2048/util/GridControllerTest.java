c08b6c55835092d02abeb27ba25276ed2ad326a1
Matas <matas.malickas@stud.vgtu.lt>
2020-10-26 15:23:34 +0200

    UPD: Test set up method.

--- src/test/java/com/lucky/game2048/util/GridControllerTest.java
+++ src/test/java/com/lucky/game2048/util/GridControllerTest.java
@@ -10,12 +10,17 @@ import java.util.ArrayList;
 
 public class GridControllerTest extends TestCase {
 
-    public void testMovementLeft() {
+    private Grid grid;
+
+    @Override
+    public void setUp() {
         int windowWidth = 600;
         int windowHeight = 800;
-        Grid grid = new Grid(4, new ArrayList<Tile>(),
+        this.grid = new Grid(4, new ArrayList<Tile>(),
                 new Dimension(windowWidth, windowHeight - 200));
+    }
 
+    public void testMovementLeft() {
         grid.replaceTile(grid.getTileAt(2, 0), new Tile(2, 0, 4));
         grid.replaceTile(grid.getTileAt(0, 2), new Tile(0, 2, 2));
         grid.replaceTile(grid.getTileAt(2, 2), new Tile(2, 2, 2));
@@ -27,11 +32,6 @@ public class GridControllerTest extends TestCase {
     }
 
     public void testMovementUp() {
-        int windowWidth = 600;
-        int windowHeight = 800;
-        Grid grid = new Grid(4, new ArrayList<Tile>(),
-                new Dimension(windowWidth, windowHeight - 200));
-
         grid.replaceTile(grid.getTileAt(0, 0), new Tile(0, 0, 2));
         grid.replaceTile(grid.getTileAt(2, 0), new Tile(2, 0, 4));
         grid.replaceTile(grid.getTileAt(2, 1), new Tile(2, 1, 2));
